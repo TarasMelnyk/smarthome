4d17d71843310fdc2db736ea94f689d714dc4202
TarasMelnyk <tarasmelnyk.kpi@gmail.com>
2016-03-10 14:50:46 +0200

    index and js

---
+++ script.js
@@ -0,0 +1,39 @@
+// document.writeln("TEXT");
+/*		var x, y, sum;
+		x=5;
+		y=7;
+		sum = x+y;
+		document.write(x + "+" + y +"=" + (sum) + "<br/>");
+		x++
+		document.write(x);
+		// alert(sum);
+		document.write(x>y);
+		var obj  = { 			
+			prop0: "</br>",
+			prop1: "text from object",
+			prop2: 1.23,
+			prop3: true 	
+				};
+		document.write(obj.prop0 +"" +obj.prop1 +" "+ obj.prop2 +" "+	 obj.prop3);*/
+/*		var arr = [1,2,'str', {}];
+		document.write("<br/>"+arr[3]);
+		arr[5] = "six_element";
+		document.write("<br/>"+arr);
+		console.log(arr);
+		delete arr [1];
+		console.log(arr);
+		document.write("<br/>"+arr);*/
+//oalert("Please input value for counters!!!");
+
+/*var x,
+	y,
+	z;
+x = 5;
+y = 10;
+z = 15;
+
+
+console.log(x);*/
+
+
+document.writeln(x);
