e3566e593ab8e69839a6169f5e828619f0ed6bf2
TarasMelnyk <tarasmelnyk.kpi@gmail.com>
2016-03-16 16:35:31 +0200

    JS_study

--- script.js
+++ script.js
@@ -36,3 +36,63 @@ z = 15;
 console.log(x);*/
 
 //document.writeln(x);
+
+
+
+/*var age = 22;
+if (age === 18) {
+	alert('Тобі 18');
+}
+else if (age === 21) {
+	alert('Тобі 21');
+}
+else {
+	alert("Gagaga")
+}*/
+
+
+/*
+(age === 18) ? alert('Тобі 18') :
+	(age === 21) ? alert('Тобі 21'):
+	(age === 24) ? alert('Тобі 24'):
+					alert("gagaga")*/
+
+
+/*
+|| или
+&& и
+!== не
+*/
+
+/*var age = 18,
+	name = "Taras";
+
+
+if (age !==18 && name === "Taras") {
+	alert("Nice");
+
+} else {
+	alert("Gagaga");
+}
+*/
+
+var number = 123.1111;
+
+// console.log( number.toFixed(2)); кільскість сиволів після коми
+
+//console.log(number.toExponential()); експонетний вид
+
+//console.log(number.toPrecision(2)); загальна кількість символів
+
+//console.log(typeof number.toString()); привести до стрінга
+
+//console.log( Math.sqrt(81)); //корінь з 81
+//console.log( Math.pow(5, 2)); //5 в 2 степені
+//console.log( Math.floor(number)); округлення вниз
+//console.log( Math.ceil(number)); округлення вверх
+//console.log( Math.round(number)); округлення до найближчого
+//console.log( Math.PI);
+
+//console.log(number/2); //NaN
+
+//console.log( isNaN(number)); // true or fals перевірка на нан
